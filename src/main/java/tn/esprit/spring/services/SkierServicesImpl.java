55997301e5e423d65080178fb84f2dbf58dd5c82
MahmoudAbdulkareem <100515746+MahmoudAbdulkareem@users.noreply.github.com>
2025-04-17 21:53:26 +0100

    ..

--- src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
@@ -45,14 +45,14 @@ public class SkierServicesImpl implements ISkierServices {
         return skierRepository.save(skier);
     }
 
-    @Override
+   /* @Override
     public Skier assignSkierToSubscription(Long numSkier, Long numSubscription) {
         Skier skier = skierRepository.findById(numSkier).orElse(null);
         Subscription subscription = subscriptionRepository.findById(numSubscription).orElse(null);
         skier.setSubscription(subscription);
         return skierRepository.save(skier);
     }
-
+*/
     @Override
     public Skier addSkierAndAssignToCourse(Skier skier, Long numCourse) {
         Skier savedSkier = skierRepository.save(skier);
